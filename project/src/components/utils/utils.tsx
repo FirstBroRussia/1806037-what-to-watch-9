4ac3b68912712a96f68fbd26603860444cf15308
FirstBroRussia <chipanrussia@gmail.com>
2022-03-03 14:54:25 +0300

    Исправлена функция

--- project/src/components/utils/utils.tsx
+++ project/src/components/utils/utils.tsx
@@ -1,6 +1,6 @@
 import {RatingLevelCountValue, RatingLevel} from '../utils/const';
 
-function getRatingLevel (ratingCount: number): any {
+function getRatingLevel (ratingCount: number): string {
   if (RatingLevelCountValue.Zero <= ratingCount && ratingCount < RatingLevelCountValue.Three) {
     return RatingLevel.Bad;}
   if (RatingLevelCountValue.Three <= ratingCount && ratingCount < RatingLevelCountValue.Five) {
@@ -10,16 +10,9 @@ function getRatingLevel (ratingCount: number): any {
   if (RatingLevelCountValue.Eight <= ratingCount && ratingCount < RatingLevelCountValue.Ten) {
     return RatingLevel.VeryGood;}
   if (ratingCount === RatingLevelCountValue.Ten) {
-    return RatingLevel.Awesome;}
-
-  // switch (ratingCount) {
-  //   case (RatingLevelCountValue.Zero <= ratingCount && ratingCount < RatingLevelCountValue.Three): return RatingLevel.Bad;
-  //   case (RatingLevelCountValue.Three <= ratingCount && ratingCount < RatingLevelCountValue.Five): return RatingLevel.Normal;
-  //   case (RatingLevelCountValue.Five <= ratingCount && ratingCount < RatingLevelCountValue.Eight): return RatingLevel.Good;
-  //   case (RatingLevelCountValue.Eight <= ratingCount && ratingCount < RatingLevelCountValue.Ten): return RatingLevel.VeryGood;
-  //   case (ratingCount === RatingLevelCountValue.Ten): return RatingLevel.Awesome;
-  //   default: return '';
-  // }
+    return RatingLevel.Awesome;} else {
+    throw new Error('ERROR');
+  }
 }
 
 function getStarringArrayToString (starring: string[]): string {
