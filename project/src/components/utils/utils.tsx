91ebbc76ebc7aa5544dfc511a4ec182a2a289330
FirstBroRussia <chipanrussia@gmail.com>
2022-03-07 10:19:02 +0300

    Модернизация утилитарной функции

--- project/src/components/utils/utils.tsx
+++ project/src/components/utils/utils.tsx
@@ -1,17 +1,13 @@
 import {RatingLevelCountValue, RatingLevel} from '../utils/const';
 
 function getRatingLevel (ratingCount: number): string {
-  if (RatingLevelCountValue.Zero <= ratingCount && ratingCount < RatingLevelCountValue.Three) {
-    return RatingLevel.Bad;}
-  if (RatingLevelCountValue.Three <= ratingCount && ratingCount < RatingLevelCountValue.Five) {
-    return RatingLevel.Normal;}
-  if (RatingLevelCountValue.Five <= ratingCount && ratingCount < RatingLevelCountValue.Eight) {
-    return RatingLevel.Good;}
-  if (RatingLevelCountValue.Eight <= ratingCount && ratingCount < RatingLevelCountValue.Ten) {
-    return RatingLevel.VeryGood;}
-  if (ratingCount === RatingLevelCountValue.Ten) {
-    return RatingLevel.Awesome;} else {
-    throw new Error('ERROR');
+  switch (true) {
+    case (RatingLevelCountValue.Zero <= ratingCount && ratingCount < RatingLevelCountValue.Three): return RatingLevel.Bad;
+    case (RatingLevelCountValue.Three <= ratingCount && ratingCount < RatingLevelCountValue.Five): return RatingLevel.Normal;
+    case (RatingLevelCountValue.Five <= ratingCount && ratingCount < RatingLevelCountValue.Eight): return RatingLevel.Good;
+    case (RatingLevelCountValue.Eight <= ratingCount && ratingCount < RatingLevelCountValue.Ten): return RatingLevel.VeryGood;
+    case (ratingCount === RatingLevelCountValue.Ten): return RatingLevel.Awesome;
+    default: throw new Error('ERROR');
   }
 }
 
